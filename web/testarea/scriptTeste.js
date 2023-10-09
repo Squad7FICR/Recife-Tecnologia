ed0dd641b8ec533b14a25e4d134c0073cf982071
skunbydev <skunbydev@gmail.com>
2023-10-09 15:46:13 -0300

    menu mobile v 1.1

--- web/testarea/scriptTeste.js
+++ web/testarea/scriptTeste.js
@@ -1,9 +1,7 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.getElementById('hamburger');
-    const navMenu = document.getElementById('ul-menu');
-    
-    hamburger.addEventListener('click', function () {
-        navMenu.classList.toggle('active');
-        hamburger.classList.toggle('closed');
-    });
+const hamburger = document.getElementById('hamburger');
+const mobileMenu = document.getElementById('mobile-menu');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('closed');
+    mobileMenu.classList.toggle('active');
 });
