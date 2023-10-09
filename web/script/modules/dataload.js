f286675f1a9a1180170594e460b38de81f79593a
skunbydev <skunbydev@gmail.com>
2023-10-08 22:49:48 -0300

    ajuste responsive menu

--- web/script/modules/dataload.js
+++ web/script/modules/dataload.js
@@ -1,4 +1,4 @@
-
+export default function InitdataLoad() {
 function exibirDataAtual() {
 
     var dataAtual = new Date();
@@ -25,3 +25,4 @@ window.onload = function() {
     exibirDataAtual();
     exibirHoraAtual();
 };
+}
--- web/script/script.js
+++ web/script/script.js
@@ -1,3 +1,5 @@
 import InitvalidationLogin from './modules/validationLogin.js';
+import InitdataLoad from './modules/dataload.js'
 
 InitvalidationLogin();
+InitdataLoad();
